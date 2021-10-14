705960187e2eb8698c02a9abf6fd27bd7e47b101
David-Leatham <david@leatham.de>
2021-10-14 17:14:13 +0200

    python envirenment

--- server.js
+++ server.js
@@ -6,15 +6,21 @@ const app = express();
 
 app.get("/", (req, res) => {
   var dataToSend = "";
-  const python = spawn("python", ["test.py"]);
+  const python = spawn("backTest/venv/Scripts/python", ["backTest/main.py"]);
   python.stdout.on("data", (data) => {
     console.log("Fetching data from Python");
+    console.log(data.toString());
     dataToSend = data.toString();
   });
+  python.stderr.on("data", (error) => {
+     console.log(error.toString());
+   });
   python.on("close", (code) => {
     // console.log(`${code}`); if you want the closing code
     // respond to Browser
     res.send(dataToSend);
+    console.log("closed")
+    
   });
 });
 
